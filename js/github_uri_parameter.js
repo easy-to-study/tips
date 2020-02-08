94964c2e1fd4d9924c200a8e0a034728f76f3456
sataga <t.g.s.0928@gmail.com>
2020-02-09 06:56:00 +0900

    ゴミ消し忘れた

--- js/github_uri_parameter.js
+++ js/github_uri_parameter.js
@@ -4,9 +4,7 @@ $(function () {
         var title = $('#id').val();
         var body = $('#body').val();
         var labels = "bug,hoge-Support,確認中/tier1";
-        console.log(body);
         var url = "https://github.com/easy-to-study/github_test/issues/new?labels="+ labels +"&title=" + title + "&body=" + body;
-        // $url = $(this).attr('href');
         window.open(encodeURI(url), '_blank');
 
     });
