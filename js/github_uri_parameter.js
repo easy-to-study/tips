8df23d7213142d5afccdcf41eabeaec21295a7a7
sataga <t.g.s.0928@gmail.com>
2020-02-09 13:22:50 +0900

    クリップボードへのコピーで解決

--- js/github_uri_parameter.js
+++ js/github_uri_parameter.js
@@ -1,10 +1,19 @@
 $(function () {
     $('#submit').click(function (event) {
         console.log("クリックされましたよ");
+
+        $('#body').select();
+        document.execCommand('copy');
+
         var title = $('#id').val();
         var body = $('#body').val();
+        var assignees = "sataga";
         var labels = "bug,hoge-Support,確認中/tier1";
-        var url = "https://github.com/easy-to-study/github_test/issues/new?labels="+ labels +"&title=" + title + "&body=" + body;
+        var projects = "easy-to-study/github_test/1"
+        var url = "https://github.com/easy-to-study/github_test/issues/new?labels=" + labels + "&title=" + title + "&projects=" + projects + "&assignees=" + assignees;
+        var encode_uri = encodeURI(url);
+        console.log("before: " + url.length)
+        console.log("after : " + encode_uri.length);
         window.open(encodeURI(url), '_blank');
 
     });
