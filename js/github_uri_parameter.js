e6609883c65d7245a78882e541f82542742c1ed0
sataga <t.g.s.0928@gmail.com>
2020-02-12 06:19:23 +0900

    クリップボード処理の確認

--- js/github_uri_parameter.js
+++ js/github_uri_parameter.js
@@ -6,7 +6,7 @@ $(function () {
         document.execCommand('copy');
 
         var title = $('#id').val();
-        var body = $('#body').val();
+        // var body = $('#body').val();　// URLに渡せないためコピペすることにした
         var assignees = "sataga";
         var labels = "bug,hoge-Support,確認中/tier1";
         var projects = "easy-to-study/github_test/1"
@@ -18,8 +18,37 @@ $(function () {
 
     });
 
+    // clipboard.jsの基本的な使い方
     new ClipboardJS('.btn', {
         container: document.getElementById('bar')
     });
 
+    $(document).on("click", "#copy", function () {
+        console.log("コピーしたよ");
+        // コピー元を取得
+        let copyTarget = $('#body').val();
+        console.log(copyTarget);
+        // コピー内容を一時的に格納するtextareaオブジェクトを作成する.
+        let copyInput = $('<textarea></textarea>');
+        // ★★CSSをうまく設定しないと見えてしまいます★★
+        copyInput.css({ 'position': 'absolute', 'right': '0' });
+        copyInput.val(copyTarget);
+
+        // textareaオブジェクトをbodyの最後に追加する.
+        $('body').append(copyInput);
+
+        // textareaオブジェクトの内容を選択する.
+        copyInput.focus();
+        copyInput.select();
+
+        // 選択したものをコピーする.
+        document.execCommand('copy');
+
+        // textareaオブジェクトを削除する.
+        copyInput.remove();
+    });
 });
+
+
+
+
