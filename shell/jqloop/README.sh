402862253b782985493050d08885e6d4eb5c1c78
sataga <t.g.s.0928@gmail.com>
2020-03-26 23:19:08 +0900

    冒険書の保存

---
+++ shell/jqloop/README.sh
@@ -0,0 +1,22 @@
+# jqloop & gitfeed
+
+## 事前準備
+GithubAccessTokenを.envに入れてください。
+```
+SOURCE_TOKEN=xxxxxxxxxxxxxxx
+```
+
+## 作業メモ
+- tokenがあれば、ユーザー設定しなくてもcloneができるのか？
+  - できる！
+```
+% docker run -it alpine-git  
+..(tokenをexportして〜)..
+/ # git clone https://oauth2:${SOURCE_TOKEN}@github.com/easy-to-study/git-feed-test.git
+Cloning into 'git-feed-test'...
+remote: Enumerating objects: 6, done.
+remote: Counting objects: 100% (6/6), done.
+remote: Compressing objects: 100% (4/4), done.
+remote: Total 6 (delta 1), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (6/6), done.
+``` 
--- shell/jqloop/script.sh
+++ shell/jqloop/script.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-set -o nounset -o errexit -o pipefail
 
+export $(cat .env)
+echo $SOURCE_TOKEN
 
+json=$(cat sample.json)
+len=$(echo $json | jq length)
+
+echo $json
+echo $len
+
+for i in $( seq 0 $(($len - 1)) ); do
+    row=$(echo $json | jq .[$i])
+    
+    name=$(echo $row | jq '.name')
+    source=$(echo $row | jq '.source')
+    destination=$(echo $row | jq '.destination')
+    echo $name 
+    echo $source
+    echo $destination
+done
