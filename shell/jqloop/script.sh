4bdee210ca8e13b7f59a7821fa9774f031cd3812
sataga <t.g.s.0928@gmail.com>
2020-03-27 00:43:26 +0900

    スクリプトの絶対パスを変数に入れておく

--- shell/jqloop/script.sh
+++ shell/jqloop/script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo `dirname $0`
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+echo $SCRIPT_DIR
+
+
 export $(cat .env)
 echo $SOURCE_TOKEN
 
@@ -12,10 +17,21 @@ echo $len
 for i in $( seq 0 $(($len - 1)) ); do
     row=$(echo $json | jq .[$i])
     
-    name=$(echo $row | jq '.name')
-    source=$(echo $row | jq '.source')
-    destination=$(echo $row | jq '.destination')
+    name=$(echo $row | jq -r '.name')
+    source=$(echo $row | jq -r '.source')
+    destination=$(echo $row | jq -r '.destination')
+    
     echo $name 
     echo $source
     echo $destination
+
+    # cd work
+    # git clone $source
+    # #pull & push部
+    # git pull $source
+    # git merge --allow-unrelated-histories $source/master
+
+    # git push -u origin master
 done
+
+cd ..
